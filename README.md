3f1e1c469339e3f3331291ab4bca393d40fe023d
Moulaye Abderrahmane ELI MBITALEB <m.elimbitaleb@etu.enset-media.ac.ma>
2023-03-10 17:38:39 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Java Hashicorp Vault Example
+
+This project is part of the tutorial on Camelcodes.net blog post :
+https://camelcodes.net/how-to-manage-java-application-secrets-using-vault
---
+++ src/net/camelcodes/vault/Main.java
@@ -0,0 +1,18 @@
+package net.camelcodes.vault;
+
+import java.util.logging.Logger;
+
+public class Main {
+
+  Logger log = Logger.getLogger(Main.class.getName());
+
+  public Main() {
+
+  }
+
+  public static void main(String[] args) {
+    new Main();
+  }
+
+
+}
